d58e746dca347a9b370f77a3691b3f743b31ec29
arrianne <arrianneoshea@hotmail.co.uk>
2017-07-03 13:47:40 +0100

    inital commit

---
+++ js/app.js
@@ -0,0 +1,17 @@
+console.log('JS loaded');
+
+
+$(document).ready(function(){
+  $('.heading').hide(0).fadeIn(3000);
+});
+
+// 
+// $(document).ready(function() {
+//   var sideslider = $('[data-toggle=collapse-side]');
+//   var sel = sideslider.attr('data-target');
+//   var sel2 = sideslider.attr('data-target-2');
+//   sideslider.click(function(){
+//     $(sel).toggleClass('in');
+//     $(sel2).toggleClass('out');
+//   });
+// });
